024e028f1e2accc99ba5b6b719e908e701854776
Alex021084 <alexander.geestmann@gmx.de>
2026-09-23 12:15:51 +0200

    Update cloud-config.js

--- cloud-config.js
+++ cloud-config.js
@@ -2,6 +2,6 @@
 /* Cloud-Zugang der Kleiderbörse.
    Nach dem Anlegen des Supabase-Projekts hier URL und anon/public key eintragen. */
 window.KB_CONFIG = {
-  supabaseUrl: "https://qzibenlrztteztmxprix.supabase.com",
+  supabaseUrl: "https://qzibenlrztteztmxprix.supabase.co",
   supabaseAnonKey: "sb_publishable_O17DosH_j1IK6CzNTeOzBQ_pdsqBQbS"
 };
